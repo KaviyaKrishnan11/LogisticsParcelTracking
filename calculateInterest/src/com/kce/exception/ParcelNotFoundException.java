2fe4e3180e427156d19086c902ce64816616f335
KaviyaKrishnan11 <717823p225@kce.ac.in>
2025-09-23 00:07:04 +0530

    Delete calculateInterest directory

--- calculateInterest/src/com/kce/exception/ParcelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.kce.exception;
-
-public class ParcelNotFoundException extends RuntimeException {
-    public ParcelNotFoundException(String id) {
-        super("Parcel with ID " + id + " not found.");
-    }
-}
